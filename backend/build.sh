9e1c1401ba0d7dba07da2f02fd63364ddf12f6f6
Stijn Diependaele <sdiepend@gmail.com>
2025-01-02 18:19:23 +0100

    Add the creation of a super user to the backend build script, need to set the env variables

--- backend/build.sh
+++ backend/build.sh
@@ -9,5 +9,8 @@ python manage.py collectstatic --noinput
 # Run migrations
 python manage.py migrate
 
+# Create superuser (Set the DJANGO_SUPERUSER_EMAIL, DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_PASSWORD environment variables)
+python manage.py createsuperuser --noinput
+
 # Start the application (this will be overridden by Procfile)
 exec gunicorn backend.wsgi:application --forwarded-allow-ips "*"
